bdd5b2a6a20c5860b570cc4f8076e50602408057
MORGAN MEZAACHE <morgan.mezaache.pro@gmail.comgit config --global user.name MORGAN>
2023-04-14 09:27:22 +0200

    quest 3 docker

---
+++ compose/client/src/App.js
@@ -0,0 +1,16 @@
+import anya from "./anya.gif";
+
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>ANYA FAN CLUB</h1>
+        <img src={anya} className="App-logo" alt="logo" />
+      </header>
+    </div>
+  );
+}
+
+export default App;
