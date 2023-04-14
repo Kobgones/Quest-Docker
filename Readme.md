81ee4c6d44a1fa4d346f79c63ea8292beded123c
MORGAN MEZAACHE <morgan.mezaache.pro@gmail.comgit config --global user.name MORGAN>
2023-04-14 10:32:41 +0200

    Anya World

--- Readme.md
+++ Readme.md
@@ -1,3 +1,7 @@
 To run the app in DEV MODE ==> 
 
+go to compose directory and run this command ==> 
+
 docker compose -f docker-compose.dev.yml up --build
+
+Enjoy 😁
--- compose/client/src/App.js
+++ compose/client/src/App.js
@@ -1,4 +1,6 @@
 import anya from "./anya.gif";
+import yor from "./yor.png";
+import loid from "./loid.png";
 
 import "./App.css";
 
@@ -6,7 +8,12 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>ANYA FAN CLUB</h1>
+        <div className="banner">
+          <img src={loid} className="loid" alt="loid forger" />
+
+          <h1>ANYA FAN CLUB</h1>
+          <img src={yor} className="yor" alt="yor forger" />
+        </div>
         <img src={anya} className="App-logo" alt="logo" />
       </header>
     </div>
